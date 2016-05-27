505c988822113144f38991fd174aafcbef44bfa6
root <root@ip-172-31-108-12.(none)>
2016-05-27 07:03:21 +0000

    Updated localusers module

--- localusers/manifests/init.pp
+++ localusers/manifests/init.pp
@@ -1,3 +1,10 @@
 class localusers {
-
+  user { 'admin':
+    ensure     => present,
+    shell      => '/bin/bash',
+    home       => '/home/admin',
+    gid        => 'wheel',
+    managehome => true,
+    password   => '$6$3Jnj/ZG7$sYEj2CDZj101LSLLbW7ErbRPdkYMlTW25ZSvtHRLC/g7YSxWf9O6kTf2Bk3DdWiWNS2hKc2b0xQrr5UU5J7uM.',
+  }
 }
